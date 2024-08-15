a61fa48af9d5cb625c341c161e9b6918d0a5d4fe
Curry4182 <kangbk4182@naver.com>
2024-08-15 16:50:02 +0900

    feat: TodoHeader 구현

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <AppHeader></AppHeader>
+  <TodoHeader></TodoHeader>
   <TodoInput></TodoInput>
   <TodoList></TodoList>
   <TodoFooter></TodoFooter>
@@ -7,14 +7,14 @@
 
 <script>
 
-import AppHeader from './components/AppHeader.vue';
+import TodoHeader from './components/TodoHeader.vue';
 import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
   components: {
-    AppHeader,
+    TodoHeader,
     TodoInput,
     TodoList,
     TodoFooter
@@ -24,12 +24,22 @@ export default {
 
 import AppVue
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #f6f6f6;
 }
+
+input {
+  border-style: groove;
+  width: 200px;
+}
+
+button {
+  border-style: groove;
+}
+
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+
 </style>
